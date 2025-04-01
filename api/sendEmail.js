7c1f75f3161281667dc554cf30eb3f883809c32b
kritgarb <garbkrit@gmail.com>
2025-04-01 17:28:24 -0300

    chore(vercel): mover chave da API do MailerSend para variável de ambiente

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -20,8 +20,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.mailersend.com/v1/email", {
       method: "POST",
       headers: {
-        Authorization:
-          "Bearer ${process.env.MAILERSEND_API_KEY}",
+        Authorization: `Bearer ${process.env.MAILERSEND_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
